3bdf8d12410b9dd72a1e7e05363166b2b032f526
maria9ballen09 <123258528+maria9ballen09@users.noreply.github.com>
2023-06-01 17:17:15 -0600

    graficas

--- js/grafica.js
+++ js/grafica.js
@@ -1,3 +1,21 @@
-const primero = document.getElementById("myChart")
+const ctx = document.getElementById('myChart');
 
-const monedasDelMundo =async ()
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 3,
+        backgroundColor:["blue"],
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
